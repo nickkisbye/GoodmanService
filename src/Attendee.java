36de022715ca7d4ff14df749e0b148e1794218be
Nick Hansen <nick4630@stud.kea.dk>
2019-05-02 11:52:18 +0200

    setup

--- src/Attendee.java
+++
@@ -1,9 +0,0 @@
-public class Attendee {
-    private String name;
-    private String type;
-
-    public Attendee(String name, String type) {
-        this.name = name;
-        this.type = type;
-    }
-}
