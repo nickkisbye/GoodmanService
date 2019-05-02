a8afee4d7c823121dcf60af77792479722add401
Nick Hansen <nick4630@stud.kea.dk>
2019-05-02 13:44:00 +0200

    setup

---
+++ src/main/java/dk/goodmanservice/goodmanservice/GoodmanserviceApplication.java
@@ -0,0 +1,14 @@
+package dk.goodmanservice.goodmanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodmanserviceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodmanserviceApplication.class, args);
+    }
+
+}
