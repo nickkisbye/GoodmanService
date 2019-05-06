f81ef8a20fa57e0553d26d6a81a5fc22b95c0bad
Nick Hansen <nick4630@stud.kea.dk>
2019-05-06 11:40:18 +0200

    css opsætning

---
+++ src/main/java/dk/goodmanservice/goodmanservice/Controller/menuController.java
@@ -0,0 +1,24 @@
+package dk.goodmanservice.goodmanservice.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class menuController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/calc")
+    public String calc() {
+        return "calc";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+}
