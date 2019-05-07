4589ebb54850016103327f8640d99b327db06511
Nick Hansen <44166062+nickkisbye@users.noreply.github.com>
2019-05-07 11:41:25 +0200

    Service

--- src/main/java/dk/goodmanservice/goodmanservice/Controller/UserController.java
+++ src/main/java/dk/goodmanservice/goodmanservice/Controller/UserController.java
@@ -1,4 +1,14 @@
 package dk.goodmanservice.goodmanservice.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "dashboard/index";
+    }
+
 }
