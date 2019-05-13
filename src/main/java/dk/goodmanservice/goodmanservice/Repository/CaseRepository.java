f6757222ef7b842d6c84985520c767759bb4f7fa
Nick Hansen <44166062+nickkisbye@users.noreply.github.com>
2019-05-13 23:06:07 +0200

    CaseRepo økonomi SQL

--- src/main/java/dk/goodmanservice/goodmanservice/Repository/CaseRepository.java
+++ src/main/java/dk/goodmanservice/goodmanservice/Repository/CaseRepository.java
@@ -87,16 +87,34 @@ public class CaseRepository implements IRepository<Case> {
                 sql = "SELECT SUM(price) FROM cases WHERE fk_mode = 3";
                 break;
             case "monthly":
-                sql = "";
+                //Hvis "monthname" ikke virker, prøv "datename"
+                sql = "SELECT MONTHNAME(endDate) as MonthName, month(endDate) as MonthNumber, SUM(price) as total_price" +
+                        " FROM cases WHERE YEAR(endDate) = YEAR(current_timestamp()) AND fk_mode = 3" +
+                        " GROUP BY MONTH(endDate)" +
+                        " ORDER BY MONTH(endDate)";
                 break;
             case "yearly":
+                sql = "SELECT YEAR(endDate) as YearNumber, SUM(price) as total_price" +
+                        " FROM cases WHERE YEAR(endDate) = YEAR(current_timestamp()) AND fk_mode = 3" +
+                        " GROUP BY YEAR(endDate)" +
+                        " ORDER BY YEAR(endDate)";
                 break;
             case "average":
                 sql = "SELECT AVG(price) from cases WHERE fk_mode = 3";
                 break;
             case "employee-sale-list":
+                sql = "SELECT U.firstName, U.lastName, SUM(C.price) as total_price from junc_jobs" +
+                        " INNER JOIN users AS U ON junc_jobs.fk_employee = U.id" +
+                        " INNER JOIN cases AS C ON junc_jobs.fk_case = C.id WHERE C.fk_mode = 3" +
+                        " GROUP BY U.firstName" +
+                        " ORDER BY SUM(C.price) DESC LIMIT 10";
                 break;
             case "top-employee":
+                sql = "SELECT U.firstName, U.lastName, SUM(C.price) as total_price from junc_jobs" +
+                        " INNER JOIN users AS U ON junc_jobs.fk_employee = U.id" +
+                        " INNER JOIN cases AS C ON junc_jobs.fk_case = C.id WHERE C.fk_mode = 3" +
+                        " GROUP BY U.firstName" +
+                        " ORDER BY SUM(C.price) DESC LIMIT 1";
                 break;
         }
 
