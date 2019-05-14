7f717ba750596c7ed57da55c8c2378c845d52dbc
mabd <mabd@itu.dk>
2019-05-14 14:00:23 +0200

    404 error mapping as intended

--- src/main/java/dk/goodmanservice/goodmanservice/Controller/ErrorController.java
+++ src/main/java/dk/goodmanservice/goodmanservice/Controller/ErrorController.java
@@ -3,7 +3,7 @@ package dk.goodmanservice.goodmanservice.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,11 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     @GetMapping("/error")
-    public String handleError(HttpServletRequest request, RedirectAttributes redirect) {
-        if ((Integer) request.getAttribute("javax.servlet.error.status_code") == 404) {
-            return "error404";
-        } else if (redirect.containsAttribute("errorCode")) {
-            return "errorSql";
+    public String handleError(HttpServletRequest request, @ModelAttribute("errorCode") String errorCode, Model model) {
+        if (request.getAttribute("javax.servlet.error.status_code") != null && (Integer) request.getAttribute("javax.servlet.error.status_code") == 404) {
+            return "ErrorHandling/error404";
+        } else if (errorCode != null) {
+            model.addAttribute("errorCode", errorCode);
+            return "ErrorHandling/errorSql";
         } else {
             return "error";
         }
