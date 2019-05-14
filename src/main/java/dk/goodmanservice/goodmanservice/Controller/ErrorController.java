cac27283e3a455c6547532b1306ce3a4fe791b85
mabd <mabd@itu.dk>
2019-05-14 14:51:32 +0200

    Updated Error controller and overall handling

--- src/main/java/dk/goodmanservice/goodmanservice/Controller/ErrorController.java
+++ src/main/java/dk/goodmanservice/goodmanservice/Controller/ErrorController.java
@@ -1,25 +1,31 @@
 package dk.goodmanservice.goodmanservice.Controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-    @GetMapping("/error")
+    @RequestMapping("/error")
     public String handleError(HttpServletRequest request, @ModelAttribute("errorCode") String errorCode, Model model) {
-        if (request.getAttribute("javax.servlet.error.status_code") != null && (Integer) request.getAttribute("javax.servlet.error.status_code") == 404) {
-            return "ErrorHandling/error404";
-        } else if (errorCode != null) {
-            model.addAttribute("errorCode", errorCode);
-            return "ErrorHandling/errorSql";
-        } else {
-            return "error";
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.valueOf(status.toString());
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "ErrorHandling/error404";
+            }
         }
+        else if(!errorCode.equals("0")) {
+            model.addAttribute("errorCode", errorCode);
+        }
+        return "ErrorHandling/error";
     }
 
     @Override
