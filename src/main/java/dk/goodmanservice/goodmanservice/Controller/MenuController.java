d7f7f0f96cb2c51bc1772edf841f763bd260278b
Nick Hansen <nick4630@stud.kea.dk>
2019-05-19 20:02:15 +0200

    kunde view

--- src/main/java/dk/goodmanservice/goodmanservice/Controller/MenuController.java
+++ src/main/java/dk/goodmanservice/goodmanservice/Controller/MenuController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
@@ -108,6 +109,18 @@ public class MenuController {
             return "error";
         }
         return "dashboard/customerlist";
+    }
+
+    @GetMapping("/dashboard/kunder/{id}")
+    public String kundeView(@PathVariable("id") int id, Model model) {
+        try {
+            model.addAttribute("kunde", US.findById(id));
+            model.addAttribute("jobs", JS.customerOffers(id));
+        } catch (SQLException e) {
+            model.addAttribute("errorCode", e.getErrorCode());
+            return "error";
+        }
+        return "dashboard/kunderView";
     }
 
 }
