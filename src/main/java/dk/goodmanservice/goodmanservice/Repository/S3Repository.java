47687e99d93dd829c2522b20b2a282bef9f2443b
Nick Hansen <44166062+nickkisbye@users.noreply.github.com>
2019-05-20 22:13:34 +0200

    billede upload og delete

---
+++ src/main/java/dk/goodmanservice/goodmanservice/Repository/S3Repository.java
@@ -0,0 +1,86 @@
+package dk.goodmanservice.goodmanservice.Repository;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Date;
+
+@Repository
+public class S3Repository {
+
+    private AmazonS3 s3client;
+
+    @Autowired
+    private BucketRepository bucketRepository;
+
+    private String endpointUrl = "http://s3.eu-central-1.amazonaws.com";
+
+    private String bucketName = "goodmanservice";
+
+    private String accessKey = "";
+
+    private String secretKey = "";
+
+    @PostConstruct
+    private void initializeAmazon() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file) {
+        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public void uploadFile(MultipartFile multipartFile, int id) throws SQLException {
+
+        String fileUrl = "";
+        try {
+            File file = convertMultiPartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileTos3bucket(fileName, file);
+            file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        bucketRepository.insertImage(fileUrl, id);
+    }
+
+
+
+    public String deleteFileFromS3Bucket(String fileUrl, int id) throws SQLException {
+        bucketRepository.deleteImage(id);
+        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+        s3client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
+        return "Successfully deleted";
+    }
+
+}
--- src/main/java/dk/goodmanservice/goodmanservice/Service/BucketService.java
+++ src/main/java/dk/goodmanservice/goodmanservice/Service/BucketService.java
@@ -1,92 +1,33 @@
 package dk.goodmanservice.goodmanservice.Service;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import dk.goodmanservice.goodmanservice.Model.Image;
+import dk.goodmanservice.goodmanservice.Repository.S3Repository;
 import dk.goodmanservice.goodmanservice.Repository.BucketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.List;
 
 @Service
 public class BucketService {
 
-
-    private AmazonS3 s3client;
-
     @Autowired
     private BucketRepository bucketRepository;
 
-    private String endpointUrl = "http://s3.eu-central-1.amazonaws.com";
+    @Autowired
+    private S3Repository s3Repository;
 
-    private String bucketName = "goodmanservice";
-
-    private String accessKey = "";
-
-    private String secretKey = "";
-
-    @PostConstruct
-    private void initializeAmazon() {
-        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-        s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
-    }
-
-    private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
-    }
-
-    private String generateFileName(MultipartFile multiPart) {
-        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
-    }
-
-    private void uploadFileTos3bucket(String fileName, File file) {
-        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
+    public void deleteFileFromS3Bucket(String fileUrl, int id) throws SQLException {
+        s3Repository.deleteFileFromS3Bucket(fileUrl, id);
     }
 
     public void uploadFile(MultipartFile multipartFile, int id) throws SQLException {
-
-        String fileUrl = "";
-        try {
-            File file = convertMultiPartToFile(multipartFile);
-            String fileName = generateFileName(multipartFile);
-            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
-            uploadFileTos3bucket(fileName, file);
-            file.delete();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        bucketRepository.insertImage(fileUrl, id);
-    }
-
-
-
-    public String deleteFileFromS3Bucket(String fileUrl, int id) throws SQLException {
-        bucketRepository.deleteImage(id);
-        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
-        s3client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
-        return "Successfully deleted";
+        s3Repository.uploadFile(multipartFile, id);
     }
 
     public List<Image> fetchImages(int id) throws SQLException {
